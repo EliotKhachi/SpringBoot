90f7883b2b24e03ad9bb40ffbc91728239dc4ba2
eliot <eliotkh12@gmail.com>
2022-09-29 23:52:01 -0700

    first commit

---
+++ src/main/java/com/EliotLearnsSpring/Boot/BootApplication.java
@@ -0,0 +1,19 @@
+package com.EliotLearnsSpring.Boot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@SpringBootApplication
+public class BootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/EliotLearnsSpring/Boot/student/Student.java
@@ -0,0 +1,92 @@
+package com.EliotLearnsSpring.Boot.student;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+@Entity // For "Hibernate"
+@Table // For table in database
+public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    private long id;
+    private String name;
+    private String email;
+
+    private LocalDate dob;
+    private Integer age;
+    public Student() {
+    }
+
+    public Student(long id, String name, String email, LocalDate dob, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public Student(String name, String email, LocalDate dob, Integer age) {
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                ", dob=" + dob +
+                '}';
+    }
+}
